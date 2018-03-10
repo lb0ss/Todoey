50df4aab071424df1e2b7a925656c2e3fae93380
林博斯 <linbs91@gmail.com>
2018-03-10 12:24:28 -0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 林博斯 on 3/3/18.
+//  Copyright © 2018 Bosi (Terrence) Lin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+    
+}
